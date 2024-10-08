f220736b7c3c74d19a7499a7f3e3425a69a02b17
Rodrigo Costa <digaoevolution@gmail.com>
2024-10-08 11:41:01 -0300

    estrutura for loop

--- javascript02/main.js
+++ javascript02/main.js
@@ -200,3 +200,21 @@ while(x > 0){
     console.log(x)
     x -= j;
 }
+
+// ------- Estrutura for --------
+// o for tem uma sintaxe mais complexa, mas bas tante utilizado;
+//A instrução fica toda em uma linha proporcionando maior coontrole;
+
+for(var z = 0; z <= 10; z++){
+    console.log('repetindo for: ' +z)
+}
+var arr1 = [1, 2, 3, 4]
+//  variavel  -  condição   - atribuição
+for(var n = 0; n < arr1.length; n++){
+    console.log(arr1[n])
+}
+console.log(arr1.length)
+
+for(var k = 5; k < 100; k *=3){
+    console.log(k)
+}
