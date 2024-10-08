590e488fc919d1c25ba3fc1c135976e461087a84
Rodrigo Costa <digaoevolution@gmail.com>
2024-10-08 12:36:56 -0300

    funções

--- javascript03/main.js
+++ javascript03/main.js
@@ -18,8 +18,44 @@ while(x < 100) {
     x += 10;
 
     if(x === 60 || x === 80) {
-        console.log('continue pulou para o proximo valor aqui')
+        console.log('continue: pulou para o proximo valor')
         continue
     }
-    console.log('resultado' + x);
+    console.log('resultado: ' + x);
 }
+
+// -------Funções (function)---------
+//sao blocos de codigos reutilizaveis;
+//evitando a repetição da logica de um programa em diversas partes do codigo;
+//a função precisa ser invocada para ser executada;
+// tornando facil de dar manutenção e de ser lida;
+
+function primeiraFuncao() {
+
+    console.log('Mundo das funções')
+}
+primeiraFuncao();
+
+
+function dizerNome(nome) {
+    console.log('O nome e: ' + nome)
+}
+//reaproveitando em varias partes trocando parametros.
+dizerNome('Rodrigo0')
+dizerNome('Rodrigo1')
+dizerNome('Rodrigo2')
+dizerNome('Rodrigo3')
+//passar como variavel o paramtro para uma função.
+var nomeBdados = 'rodrigo costa'
+dizerNome(nomeBdados)
+
+//retorne
+function soma(a, b) {//sempre separado por virgula.
+    var soma = a+b;
+    return soma;
+} //proporciona receber um valor de volta da função
+var soma1 = soma(2, 5);
+console.log('resiltado ' + soma1);
+
+console.log(soma(4, 5));
+console.log(soma(10, 7));
