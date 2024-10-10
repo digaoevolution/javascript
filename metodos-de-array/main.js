8f629eba8466214105c97005bf824349b2e83d88
Rodrigo Costa <digaoevolution@gmail.com>
2024-10-10 10:51:54 -0300

    complemento parte 2

--- metodos-de-array/main.js
+++ metodos-de-array/main.js
@@ -32,3 +32,21 @@ console.log(arr[arr.length - 1]);
 console.log(Array.isArray(5));
 
 console.log(Array.isArray(arr));
+
+//splice -> extrair algo ou palavra da frase;
+//adicionar elementos no meio de um array;
+var arr1 = [1,2,3,4,5];
+// 2 = indice ... 0 = nao apaga/adiciona..... 222=numero adicionado;
+arr1.splice(2, 0, 222);
+console.log(arr1)
+//remover elemento em um array;
+//4= indice .... 1=remover 
+arr1.splice(4, 1);
+console.log(arr1);
+
+//join -> transformar um array em uma string;
+var arr2 = ['canudo', 'de', 'papael'];
+console.log(arr2.join(' '));
+
+//reverse -> inverter uma array;
+console.log(arr2.reverse())
