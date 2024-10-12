28266170d93fd35c28f87b942b4f71a86928f9cf
Rodrigo Costa <digaoevolution@gmail.com>
2024-10-12 12:25:14 -0300

    querySelector e querySelectorAll

---
+++ querySelector-e-querySelectorAll-DOM/main.js
@@ -0,0 +1,16 @@
+var itensClass = document.getElementsByClassName('item');
+console.log(itensClass)
+
+//querySelectorAll - varios elementos da lista
+var itensQuery2 = document.querySelectorAll('#lista li');
+console.log(itensQuery2);
+
+var itensQuery = document.querySelectorAll('#lista2 li');
+console.log(itensQuery);
+
+//querySelector - pegar apenas um elemento
+var lista = document.querySelector('#lista');
+console.log(lista)
+
+var span = document.querySelector('#paragrafo span');
+console.log(span)
