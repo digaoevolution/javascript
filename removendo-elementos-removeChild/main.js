be5c9a83256b82ee2c874be78040b074fc293cb7
Rodrigo Costa <digaoevolution@gmail.com>
2024-10-15 15:14:48 -0300

    removendo elementos

---
+++ removendo-elementos-removeChild/main.js
@@ -0,0 +1,7 @@
+//-------Removerndo elementos------
+//removendo conteudo filho
+//removeChild
+var container = document.querySelector('#container');
+var p = document.querySelector('#container p');
+container.removeChild(p);
+
